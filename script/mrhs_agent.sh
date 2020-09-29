8ff052534c54357bec922494e2580b19385b6b7a
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2020-09-30 01:25:52 +0900

    Use .mrhs.json to connect to adafruit

--- script/mrhs_agent.sh
+++ script/mrhs_agent.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-username=xxxxxxxxxx
-key=yyyyyyyyyy
 
-/usr/sbin/lsof | /usr/bin/grep VDC | /usr/bin/wc -l | /usr/local/bin/mosquitto_pub -h io.adafruit.com -p 8883 -t ${username}/feeds/mrhs -u ${username} -P ${key} --tls-version tlsv1.2 --cafile /etc/ssl/cert.pem -s
+USERNAME=`cat .mrhs.json | jq -r '.username'`
+KEY=`cat .mrhs.json | jq -r '.key'`
+
+/usr/sbin/lsof | /usr/bin/grep VDC | /usr/bin/wc -l | /usr/local/bin/mosquitto_pub -h io.adafruit.com -p 8883 -t ${USERNAME}/feeds/mrhs -u ${USERNAME} -P ${KEY} --tls-version tlsv1.2 --cafile /etc/ssl/cert.pem -s
