fd2b39ef1d8eff7f3e6b88dcdaff05b14d5c7fd0
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-01-29 23:52:25 +0900

    Apply formatter

--- mrhs/mrhs.ino
+++ mrhs/mrhs.ino
@@ -14,7 +14,7 @@
 #define READ_TIMEOUT 5000
 
 // Wi-Fi
-const char* fname = "/config.csv";
+const char *fname = "/config.csv";
 
 // config
 File fp;
@@ -26,13 +26,15 @@ char key[64];
 // adafruit
 char feed[64];
 
-void SetupWifi(const char *file) {
+void SetupWifi(const char *file)
+{
   unsigned int cnt = 0;
   char data[256];
   char *str;
 
   fp = SD.open(fname, FILE_READ);
-  while(fp.available()){
+  while (fp.available())
+  {
     data[cnt++] = fp.read();
   }
   Serial.print("data:");
@@ -40,20 +42,20 @@ void SetupWifi(const char *file) {
   Serial.print(data);
   Serial.print("\n-----\n");
 
-  strtok(data,",");
-  str = strtok(NULL,"\r");
+  strtok(data, ",");
+  str = strtok(NULL, "\r");
   strncpy(&ssid[0], str, strlen(str));
 
-  strtok(NULL,",");
-  str = strtok(NULL,"\r");
+  strtok(NULL, ",");
+  str = strtok(NULL, "\r");
   strncpy(&pass[0], str, strlen(str));
 
-  strtok(NULL,",");
-  str = strtok(NULL,"\r");
+  strtok(NULL, ",");
+  str = strtok(NULL, "\r");
   strncpy(&username[0], str, strlen(str));
 
-  strtok(NULL,",");
-  str = strtok(NULL,"\r");
+  strtok(NULL, ",");
+  str = strtok(NULL, "\r");
   strncpy(&key[0], str, strlen(str));
 
   M5.Lcd.printf("SSID:%s\n", ssid);
@@ -67,9 +69,10 @@ void SetupWifi(const char *file) {
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, pass);
-  while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
   }
   Serial.print("\n");
 
@@ -84,41 +87,48 @@ WiFiClientSecure client;
 Adafruit_MQTT_Client *mqtt;
 Adafruit_MQTT_Subscribe *onMeetingIndicator;
 
-void showOnMeeting(uint16_t bgColor) {
+void showOnMeeting(uint16_t bgColor)
+{
   M5.Lcd.fillScreen(bgColor);
   M5.Lcd.setCursor(40, 90);
   M5.Lcd.setTextSize(7);
   M5.Lcd.println(F("ON MEETING"));
 }
 
-void showOk() {
+void showOk()
+{
   M5.Lcd.fillScreen(TFT_GREEN);
   // 320 * 240
   M5.Lcd.fillEllipse(160, 120, 100, 100, TFT_WHITE);
   M5.Lcd.fillEllipse(160, 120, 75, 75, TFT_GREEN);
 }
 
-void showNg() {
+void showNg()
+{
   M5.Lcd.fillScreen(TFT_RED);
   // 320 * 240
   //M5.Lcd.fillRect(60, 110, 200, 20, TFT_WHITE);
   //M5.Lcd.fillRect(150, 20, 20, 200, TFT_WHITE);
-  for (int i=0; i <= 10; i++) {
-    M5.Lcd.drawLine(60+i, 20-i, 260+i, 220-i, TFT_WHITE);
-    M5.Lcd.drawLine(60-i, 20+i, 260-i, 220+i, TFT_WHITE);
-    M5.Lcd.drawLine(60+i, 220+i, 260+i, 20+i, TFT_WHITE);
-    M5.Lcd.drawLine(60-i, 220-i, 260-i, 20-i, TFT_WHITE);
+  for (int i = 0; i <= 10; i++)
+  {
+    M5.Lcd.drawLine(60 + i, 20 - i, 260 + i, 220 - i, TFT_WHITE);
+    M5.Lcd.drawLine(60 - i, 20 + i, 260 - i, 220 + i, TFT_WHITE);
+    M5.Lcd.drawLine(60 + i, 220 + i, 260 + i, 20 + i, TFT_WHITE);
+    M5.Lcd.drawLine(60 - i, 220 - i, 260 - i, 20 - i, TFT_WHITE);
   }
 }
 
-void MQTT_connect() {
+void MQTT_connect()
+{
   int8_t ret;
-  if (mqtt->connected()) {
+  if (mqtt->connected())
+  {
     return;
   }
 
   Serial.println(F("Connecting to MQTT... "));
-  while ((ret = mqtt->connect()) != 0) {
+  while ((ret = mqtt->connect()) != 0)
+  {
     Serial.println(F(mqtt->connectErrorString(ret)));
     Serial.println(F("Retrying MQTT connection in 5 seconds ..."));
     mqtt->disconnect();
@@ -127,7 +137,8 @@ void MQTT_connect() {
   Serial.println(F("MQTT connected!"));
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   M5.begin();
   M5.Lcd.clear(BLACK);
@@ -150,23 +161,30 @@ void setup() {
   showOk();
 }
 
-void loop() {
+void loop()
+{
   M5.update();
 
   MQTT_connect();
   Adafruit_MQTT_Subscribe *subscription;
-    while ((subscription = mqtt->readSubscription(READ_TIMEOUT))) {
-    if (subscription == onMeetingIndicator) {
-      String lastread = String((char*)onMeetingIndicator->lastread);
+  while ((subscription = mqtt->readSubscription(READ_TIMEOUT)))
+  {
+    if (subscription == onMeetingIndicator)
+    {
+      String lastread = String((char *)onMeetingIndicator->lastread);
       lastread.trim();
-      if (lastread == "") {
+      if (lastread == "")
+      {
         continue;
       }
       // Two processes are already in use before the camera was actually used.
       // (avconfere and google chrome)
-      if (lastread.toInt() <= 2) {
+      if (lastread.toInt() <= 2)
+      {
         showOk();
-      } else {
+      }
+      else
+      {
         showNg();
       }
     }
